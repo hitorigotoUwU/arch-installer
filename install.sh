2cf7f5b5a55ad3e6bff3907aea51ef108749fda7
tyler <hitorigoto0w0@gmail.com>
2023-07-16 21:13:56 +1000

    i forgot to save a file lol

--- install.sh
+++ install.sh
@@ -55,33 +55,10 @@ fi
 echo "creating the fstab ..."
 genfstab -U /mnt >> /mnt/etc/fstab
 
+# prepare for chroot
+mkdir -p /mnt/tmp
+curl https://raw.githubusercontent.com/hitorigotoUwU/arch-installer/main/part2.sh -o /mnt/tmp/install-part2.sh
+chmod +x /mnt/tmp/install-part2.sh
+
 echo "chrooting ..."
-arch-chroot /mnt
-
-echo "time & locale stuff ..."
-ln -sf /usr/share/zoneinfo/Australia/Brisbane /etc/localtime
-sed -i 's/^#en_AU.UTF-8 UTF-8/en_AU.UTF-8 UTF-8/' /etc/locale.gen
-locale-gen
-touch /etc/locale.conf && echo "LANG=en_AU.UTF-8" > /etc/locale.conf
-
-echo "what would you like the system hostname to be?"
-read HOSTNAME
-echo "$HOSTNAME" > /etc/hostname
-
-echo "creating root password. please enter a password for the root account"
-passwd 
-
-echo "creating user account."
-echo "what would you like the username to be?"
-read USERNAME
-useradd -m -G wheel -s /bin/bash $USERNAME
-
-echo "creating user password. please enter a password for the $USERNAME account"
-passwd $USERNAME
-
-echo "editing sudoers file ..."
-sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-
-echo "configuring grub ..."
-grub-install /dev/sda
-grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt /bin/bash /tmp/part2.sh
